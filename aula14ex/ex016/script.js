461185407c3b57d3a236271ba45e56ad83709832
Kleiton Marques <kleiton.88design@gmail.com>
2019-10-23 21:01:44 -0200

    ATUALIZACAO: Melhoria no método utilizando validações

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,16 +4,28 @@ function executarContagem(){
     var passo = document.querySelector("input#passo")
     var res = document.querySelector("div#res")
 
-    inicio = Number(inicio.value)
-    fim = Number(fim.value)
-    passo = Number(passo.value)
+    if(inicio.value == "" || fim.value == "" || passo.value == ""){
+        res.innerHTML = "Impossível contar!"
+    } else {
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        passo = Number(passo.value)
 
+        if(passo == 0) {
+            passo = 1
+            alertErroZero(passo)
+        }
+    
+        function alertErroZero(passo) {
+            alert(`Passo inválido! Considerando PASSO ${passo}`)
+        }
 
-    for(i = inicio; i <= fim; i+=passo) {
-        res.innerHTML += i
-        res.innerHTML += String.fromCodePoint(128073)
+        res.innerHTML = "Contando:<br>"
+        for(i = inicio; i <= fim; i+=passo) {
+            res.innerHTML += i
+            res.innerHTML += String.fromCodePoint(128073)
+        }
+        res.innerHTML += String.fromCodePoint(128681)
     }
-    res.innerHTML += String.fromCodePoint(128681)
- 
 }
 
