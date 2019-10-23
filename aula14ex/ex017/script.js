869be7000514286c3be25f31eb415d1bb2c55a23
Kleiton Marques <kleiton.88design@gmail.com>
2019-10-23 21:52:00 -0200

    EX: 2 For JS com tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-console.log("TESTANDO...")
+function gerarTabuada(){
+    var numeroTabuada = document.querySelector("input#numeroTabuada")
+    var espacoTabuada = document.querySelector("select#tabuada")
+
+    if(numeroTabuada.value == "") {
+        alert("Por favor, digite um número!")
+        numeroTabuada.focus()
+    } else {
+        espacoTabuada.setAttribute("size", "10")
+        espacoTabuada.innerHTML = ""
+        numeroTabuada = Number(numeroTabuada.value)
+        for(i = 1; i <= 10; i++) {
+            espacoTabuada.innerHTML += "<option>" + numeroTabuada + " x " + i + " = " + numeroTabuada*i + "</option>"
+            // console.log(numeroTabuada + " x " + i + " = " + numeroTabuada*i)
+        }
+        
+    }
+}
+
