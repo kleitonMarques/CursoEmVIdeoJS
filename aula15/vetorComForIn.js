c25d2d6fa41807d26ed6a861657690b55a902842
Kleiton Marques <kleiton.88design@gmail.com>
2019-10-25 18:07:30 -0200

    Trabalhando com varivavel composta --- Array

---
+++ aula15/vetorComForIn.js
@@ -0,0 +1,7 @@
+let valores = [8, 1, 7, 4, 2, 9]
+valores.sort()
+console.log(valores)
+
+for(i in valores) {
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
---
+++ aula15/vetornatela.js
@@ -0,0 +1,7 @@
+var valores = [8, 1, 7, 4, 2, 9]
+
+console.log(valores)
+
+for(i = 0; i < valores.length; i++) {
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
